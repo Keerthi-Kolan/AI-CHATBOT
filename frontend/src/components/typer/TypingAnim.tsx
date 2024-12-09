f1a7b8192be2e234a5e5d42b350687fa9e7da908
Keerthi Kolan <kolankeerthi.reddy@gmail.com>
2024-12-08 20:30:27 -0800

    all files added

---
+++ frontend/src/components/typer/TypingAnim.tsx
@@ -0,0 +1,27 @@
+import { TypeAnimation } from "react-type-animation";
+
+const TypingAnim = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed once, initially
+        "You have a question?",
+        2000,
+        "I am ready to answer",
+        3000,
+        "Signup up and let's Chat",
+        4500,
+      ]}
+      speed={50}
+      style={{
+        fontSize: "60px",
+        color: "white",
+        display: "inline-block",
+        textShadow: "1px 1px 20px #000",
+      }}
+      repeat={Infinity}
+    />
+  );
+};
+
+export default TypingAnim;
